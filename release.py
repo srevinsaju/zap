022d897f794180cad57e85fd7f92e42ecb45515d
Srevin Saju <srevinsaju@sugarlabs.org>
2020-08-31 17:34:20 +0300

    Update version and add releasing tool

---
+++ release.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from zap import __doc__
+
+print(sys.argv)
+
+if sys.argv[-1].endswith('.py'):
+    print("ERR: Invalid argument.")
+    sys.exit(-1)
+
+version = sys.argv[-1]
+with open(os.path.join('zap', '__init__.py'), 'w') as fp:
+    fp.write('"""')
+    fp.write(__doc__)
+    fp.write('"""')
+    fp.write('\n\n')
+    fp.write('__version__ = "{}"'.format(version))
+print("Released {}".format(version))
