445558be3c5cd92c6b4b9535839c31919dac854b
Srevin Saju <srevinsaju@sugarlabs.org>
2020-09-06 13:12:18 +0300

    Add zap backend

---
+++ zap-backend/app.py
@@ -0,0 +1,37 @@
+import flask
+from flask import request, Request
+import requests
+from flask import abort
+from flask import jsonify
+import json
+from zap.appimage.generator import AppImageConfigJsonGenerator
+app = flask.Flask(__name__)
+app.config['DEBUG'] = True
+
+FEED_URL = "http://appimage.github.io/feed.json"
+
+def get_feed_json():
+    feed_request = requests.get(FEED_URL)
+    return feed_request.json()
+
+def get_app_data_from_feed(appname):
+    feed = get_feed_json().get('items')
+    for appdata in feed:
+        if appdata.get('name') in (appname, appname.lower(), appname.upper()):
+            return appdata
+
+@app.route('/core/<appname>', methods=['GET'])
+def return_data(appname):
+    appdata = get_app_data_from_feed(appname)
+    if appdata is None:
+        abort(404)
+    app_core_json = AppImageConfigJsonGenerator(appdata)
+    return jsonify(
+            json.loads(
+                json.dumps(
+                    app_core_json.get_app_metadata())))
+
+
+    
+if __name__ == "__main__":
+    app.run()
