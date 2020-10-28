a725ca5fcb0c2f6feb5d198302457d473990d825
Srevin Saju <srevinsaju@sugarlabs.org>
2020-10-28 09:18:36 +0300

    fix: cURL redirect stdout to file

--- tools/install.sh
+++ tools/install.sh
@@ -44,8 +44,8 @@ main() {
     wget -O "$ZAP_ROOT/zap-x86_64.AppImage" -q --show-progress \
       https://github.com/srevinsaju/zap/releases/download/continuous/zap-x86_64.AppImage
   elif command_exists curl; then
-    curl -LJO --progress-bar -o "$ZAP_ROOT/zap-x86_64.AppImage" \
-      https://github.com/srevinsaju/zap/releases/download/continuous/zap-x86_64.AppImage
+    curl -LJO --progress-bar \
+      https://github.com/srevinsaju/zap/releases/download/continuous/zap-x86_64.AppImage > ~/.zap/zap-x86_64.AppImage
   else
     error Zap requires wget or curl to download the zap binary.
     exit 1
