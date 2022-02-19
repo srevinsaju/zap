fbb379574cb1a24d17c19db79b30795546f7267b
Srevin Saju <srevinsaju@sugarlabs.org>
2022-02-19 21:20:12 +0300

    fix: change URL to srev.in

--- desktop/main.go
+++ desktop/main.go
@@ -8,6 +8,6 @@ func main() {
 	defer w.Destroy()
 	w.SetTitle("Minimal webview example")
 	w.SetSize(800, 600, webview.HintNone)
-	w.Navigate("https://g.srevinsaju.me/get-appimage")
+	w.Navigate("https://g.srev.in/get-appimage")
 	w.Run()
 }
