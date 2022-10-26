2ee2dd0446f77b94433b2c3111e8e75f69e77cfc
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2022-10-26 22:25:00 +0300

    Update uninstall.sh
    Co-authored-by: Srevin Saju <srevinsaju@sugarlabs.org>

--- uninstall.sh
+++ uninstall.sh
@@ -18,21 +18,8 @@ echo
 REPO="srevinsaju/zap"
 
 # Uninstallation
-if [ "$EUID" -eq 0 ]; then
-	echo [~] Script is running as root.
-	echo
-	echo [~] Uninstalling System-Wide
-	sudo rm /usr/local/bin/zap
-	# Done
-	echo [~] Done....
-else
-	echo [~] Script is not running as root user
-	echo
-	echo '[~] Uninstalling Locally from ~/.local/bin/'
-	rm ~/.local/bin/zap
-	# Done
-	echo [~] Done....
-fi
+echo [~] Uninstalling zap
+rm $(which zap)
 
 # Installation Complete
 echo
